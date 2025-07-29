bcbea4328bb221876a4f89661af423b6a940a828
James Kuny <james.kuny@yahoo.com>
2025-07-29 17:02:40 -0400

    Added test for weather codes

---
+++ internal/apis/open_meteo/model/weather_code_test.go
@@ -0,0 +1,26 @@
+package model
+
+import "testing"
+
+const expectedError = "Expected error, got nil"
+
+var expectedCodes = [...]int{
+	0, 1, 2, 3, 45, 48, 51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 71, 73, 75, 77, 80, 81, 82, 85, 86, 95, 96, 99,
+}
+
+func TestValidWeatherCodes(t *testing.T) {
+	for i := range expectedCodes {
+		got := WeatherConditionsMap[expectedCodes[i]].Description
+		if got == "" {
+			t.Errorf(expectedError)
+		}
+	}
+}
+
+// TestInvalidWeatherCodes a code that isn't in the list should return nothing
+func TestInvalidWeatherCodes(t *testing.T) {
+	got := WeatherConditionsMap[1000].Description
+	if got != "" {
+		t.Errorf(expectedError)
+	}
+}
